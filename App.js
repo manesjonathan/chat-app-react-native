1d05b5c1c8b29057b88192252d4035db24a39e18
Jonathan Manes <manesjona@gmail.com>
2023-05-08 09:52:48 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Login from "./screens/Login";
+import Messaging from "./screens/Messaging";
+import Chat from "./screens/Chat";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+              name='Login'
+              component={Login}
+              options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+              name='Chat'
+              component={Chat}
+              options={{
+                title: "Chats",
+                headerShown: false,
+              }}
+          />
+          <Stack.Screen name='Messaging' component={Messaging} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
